02b357ba881c41ac2a5fac68a430388530b2ef49
Jorge <jorge.pino@tsoftglobal.com>
2024-09-05 13:17:13 -0400

    se suben primeros ejercicios de JAVA

---
+++ src/Ejercicios/Ejercicio2.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int numero1 = 7;
+        int numero2 = 8;
+
+        // Sumar los dos números
+        int producto = numero1 * numero2;
+
+        // Mostrar el resultado de la suma
+        System.out.println("El producto de  " + numero1 + " y " + numero2 + " es: " + producto);
+    }
+}
---
+++ src/Ejercicios/SumaDeDosNumeros.java
@@ -0,0 +1,15 @@
+package Ejercicios;
+
+public class SumaDeDosNumeros {
+    public static void main(String[] args) {
+        // Declarar dos números
+        int numero1 = 10;
+        int numero2 = 20;
+
+        // Sumar los dos números
+        int suma = numero1 + numero2;
+
+        // Mostrar el resultado de la suma
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
